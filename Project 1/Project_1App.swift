3441d4f5a52306541f037c713e9b0579cd2ebe05
Krow <katie.scantland@stu.glasgow.kyschools.us>
2024-09-17 08:24:29 -0500

    Initial Commit

---
+++ Project 1/Project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1App.swift
+//  Project 1
+//
+//  Created by Krow on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
